8c79b231635ea6f047683dbc16eb93491bcdfa8b
Natasha <n.chalganova@mail.ru>
2022-12-10 09:55:19 +0300

    38 правка

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -4,14 +4,16 @@
 Console.WriteLine ("Введите количество элементов в массиве: ");
 int n = Convert.ToInt32 (Console.ReadLine());
 int [] array = new int [n];
-int max = array[0];
-int min = array[0];
+
 int diff = 0;
 for (int i=0; i<n; i++)
 {
     array[i] = new Random().Next (1, 10);
     Console.Write ($" {array[i]} ");
 }
+int max = array[1];
+int min = array[1];
+
 for (int m=0; m<n; m++)
 {
     if (array[m] > max)
@@ -20,7 +22,7 @@ for (int m=0; m<n; m++)
 //Console.WriteLine ($" -->{max} ");
 for (int m=0; m<n; m++)
 {
-    if (array[m] < max)
+    if (array[m] < min)
     min = array[m];
 }  
 //Console.WriteLine ($" -->{min} ");
